39b9aabd88f39a8f1d7bc5440caa7149f9a0baf6
mknadler <miriam.k.nadler@gmail.com>
2024-04-21 22:33:30 -0400

    disable strict ts because fuck ts

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,10 +4,10 @@ import { reader } from '$lib/reader';
 export const prerender = true;
 
 export async function load() {
-	const posts = await reader.collections.posts.all({ resolveLinkedFiles: true });
+	const events = await reader.collections.events.all({ resolveLinkedFiles: true });
 
-	if (posts) {
-		return { posts };
+	if (events) {
+		return { events };
 	}
 
 	error(404, 'Not found');
--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -4,10 +4,10 @@ import { reader } from '$lib/reader';
 export const prerender = true;
 
 export async function load({ params }) {
-	const post = await reader.collections.posts.read(params.slug, { resolveLinkedFiles: true });
+	const event = await reader.collections.events.read(params.slug, { resolveLinkedFiles: true });
 
-	if (post) {
-		return post;
+	if (event) {
+		return event;
 	}
 
 	error(404, 'Not found');
