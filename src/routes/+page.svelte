a98ab840ad20464765fe1a67d7250868d47a58f9
mknadler <miriam.k.nadler@gmail.com>
2024-04-21 23:04:01 -0400

    Display some data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
   export let data
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Keystatic posts</h2>
+<h1>South Slope Pinball</h1>
+<p>Work in progress</p>
+<h2>Events</h2>
 
 {#each data.events as event}
-  <h3><a href="/events/{event.slug}">{event.eventName}</a></h3>
+  {JSON.stringify(event, 2, null)}
+  <div style="background: #EEEEEE; padding: 1rem;">
+
+  <h3><a href="/events/{event.slug}">{event.entry.eventName}</a></h3>
+  </div>
 {/each}
--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -3,5 +3,13 @@
 </script>
 
 <a href="/">&larr; Back home</a>
-<h1>{data.title}</h1>
-<pre>{JSON.stringify(data.content, null, 2)}</pre>
+<h1>{data.eventName}</h1>
+{data.description[0].children[0].text}
+
+<br/>
+starts at: {data.starttime}
+<br/>
+doors at: {data.doorstime}
+<br/>
+url: <a href="#">{data.matchplayURL}</a><br/>
+location: <a href="/venues/{data.location}">{data.location}</a>
