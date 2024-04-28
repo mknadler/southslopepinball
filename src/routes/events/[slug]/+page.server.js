893d0a08e16dfb8eea26044e9f87da7c7d744b2a
mknadler <miriam.k.nadler@gmail.com>
2024-04-28 18:51:04 -0400

    Render markdown for descriptions

--- src/routes/events/[slug]/+page.server.js
+++ src/routes/events/[slug]/+page.server.js
@@ -1,12 +1,14 @@
 import { error } from '@sveltejs/kit';
 import { reader } from '$lib/reader';
+import { compile } from 'mdsvex';
 
 export const prerender = true;
 
 export async function load({ params }) {
 	const event = await reader.collections.events.read(params.slug, { resolveLinkedFiles: true });
+	let compiledDescription = await compile(event.description);
 	// @ts-ignore
-	event.description = JSON.stringify(event.description)
+	event.description = { content: compiledDescription?.code };
 	
 	const location = await reader.collections.locations.read(event.location, { resolveLinkedFiles: true});
 	if (event) {
--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -1,16 +1,11 @@
 <script>
-  import mdocc from '@markdoc/markdoc';
-  import Markdoc from 'sveltejs-markdoc';
-  export let data
+  // import mdocc from '@markdoc/markdoc';
+  // import Markdoc from 'sveltejs-markdoc';
+
+  export let data;
   let event = data.event;
   let location = data.location;
 
-
-  let source = event.description;
-  let ast = mdocc.parse(source);
-  let content = mdocc.transform(ast);
 </script>
 
-{event.description}
-
-<Markdoc content={JSON.stringify(content)} />
+{@html event.description.content}
