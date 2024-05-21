2acb484b9372f74397262c1b674a220dad3ff3a8
mknadler <miriam.k.nadler@gmail.com>
2024-05-21 18:03:49 -0400

    Add mock data for event/series/etc for use in storybook stories

--- src/lib/components/eventcard/EventCard.stories.ts
+++ src/lib/components/eventcard/EventCard.stories.ts
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
 import EventCard from '$lib/components/eventcard/EventCard.svelte';
+import { mockLocationObject } from '$lib/types/LocationObject';
+import { mockSeriesObject } from '$lib/types/SeriesObject';
+import { mockEventObject } from '$lib/types/EventObject';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
@@ -16,5 +19,8 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
+    locationObject: mockLocationObject,
+    seriesObject: mockSeriesObject,
+    eventObject: mockEventObject
   },
 };
--- src/lib/types/TextObjectTypes.ts
+++ src/lib/types/TextObjectTypes.ts
@@ -9,5 +9,25 @@ export type TextObject = {
 
 export type TextBlockObject = {
     children: TextObject[],
-    type: string // e.g. 'paragraph', 'heading'
+    type?: string // e.g. 'paragraph', 'heading'
+}
+
+export const mockTextObjectShort = {
+    text: 'Lorem ipsum sic dolor amet.'
+}
+
+export const mockTextObjectLong = {
+    text: "On the other hand, we denounce with righteous indignation and dislike men " +
+    "who are so beguiled and demoralized by the charms of pleasure of the moment, so " +
+    "blinded by desire, that they cannot foresee the pain and trouble that are bound to " +
+    "ensue; and equal blame belongs to those who fail in their duty through weakness of "+
+    "will, which is the same as saying through shrinking from toil and pain."
+}
+
+export const mockTextBlockObjectShort = {
+    children: [mockTextObjectShort]
+}
+export const mockTextBlockObjectLong = {
+    children: [mockTextObjectShort, mockTextObjectLong, mockTextObjectShort],
+    type: 'paragraph'
 }
