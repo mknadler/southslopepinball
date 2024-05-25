d9ec9abf3eabce5110800ab01e06dd59fe52057e
mknadler <miriam.k.nadler@gmail.com>
2024-05-25 17:43:11 -0400

    Add HTTP prefixing to cms-sourced links

---
+++ src/lib/utils/prefixHttp.ts
@@ -0,0 +1,10 @@
+// Vite and/or sveltekit tries to make links like `<a href="google.com"/>` 
+// into `<a href="southsloppinball.com/google.com">`, and I want users to be able to dump in
+// e.g. `matchplay.events/event/12345` as the url in keystatic without the build breaking
+export const prefixHTTP = (url) => {
+    if (url.match(/(http:\/\/)|(https:\/\/)/)) {
+        return url;
+    } else {
+        return `http://` + url;
+    }
+}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,6 +5,8 @@ import type { EventObject } from '$lib/types/EventObject';
 import type { LocationObject } from '$lib/types/LocationObject';
 import type { SeriesObject } from '$lib/types/SeriesObject';
 
+import { prefixHTTP } from '$lib/utils/prefixHttp';
+
 export const prerender = true;
 
 export async function load() {
@@ -12,6 +14,18 @@ export async function load() {
 	const locations: LocationObject[] = await reader.collections.locations.all({ resolveLinkedFiles: true});
 	const machines = await reader.collections.machines.all({ resolveLinkedFiles: true});
 	const series: SeriesObject[] = await reader.collections.series.all({ resolveLinkedFiles: true});
+
+  events.forEach(eventObject => {
+      if (eventObject.entry.matchplayURL) {
+          eventObject.entry.matchplayURL = prefixHTTP(eventObject.entry.matchplayURL);
+      }
+  })
+  locations.forEach(location => {
+      if (location.entry.url) {
+          location.entry.url = prefixHTTP(location.entry.url);
+      }
+  })
+
 	if (events && locations && machines && series) {
 		return { events, locations, machines, series };
 	}
